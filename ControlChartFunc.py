864afd89f8037bcb329c453a46dcab7ff4018de0
Hanjie Liang <hanjieliang0817@gmail.com>
2023-06-28 14:32:45 +0100

    Add a new class that could compute robust mean in various ways in ControlChartFunc.py and implement it in the main

--- ControlChartFunc.py
+++ ControlChartFunc.py
@@ -1,5 +1,7 @@
 import warnings
 import numpy as np
+from bisect import insort
+from scipy.signal import tukey
 
 class ControlChart:
     """
@@ -218,3 +220,158 @@ class ControlChart:
         return ind
 
 
+class RobustMethods:
+    """
+    This class implements various robust methods for computing the mean of streaming data, including trimmed mean, winsorized mean, and cosine tapered mean. It also provides a method for computing the sliding window median of the data.
+
+    Attributes:
+        x (np.ndarray): The input streaming data. It's a fixed dataset, but the values are coming in a stream.
+        n (int): The number of observations in the data.
+
+    Methods:
+        compute_mean_sequence(trimmed_ratio:float, winsorized_ratio:float, cosine_ratio:float) -> dict:
+            Computes the mean sequence of the data using three robust methods: trimmed mean, winsorized mean, and cosine tapered mean.
+
+        sliding_window_median(window_length:int) -> list:
+            Computes the median of a sliding window over the data.
+
+    Private Methods:
+        __insert_next_for_sorted_data() -> None:
+            Inserts the next value from the data stream into the sorted list.
+
+        __trimmed_mean(ratio:float) -> float:
+            Calculates the trimmed mean of the sorted data.
+
+        __winsorized_mean(ratio:float) -> float:
+            Calculates the winsorized mean of the sorted data.
+
+        __cosine_tapered_mean(ratio:float) -> float:
+            Calculates the cosine tapered mean of the sorted data.
+    """
+    def __init__(self, x: np.ndarray):
+        """
+        Initialize the ControlChart class with input data.
+
+        Parameters:
+        x (np.array): Input streaming data (fixed data, but coming in a stream).
+        """
+        assert isinstance(x, np.ndarray), f"Input={x} must be a numpy array"
+        self.x = x
+        self.n = len(x) # number of observation
+        self._current_ind = 0
+        self._sorted_data = []
+
+    def __insert_next_for__(self):
+        """
+        Insert the next value from the data stream into the sorted list and sort it.
+        """
+        if self._current_ind < self.n:
+            insort(self._sorted_data, self.x[self._current_ind])
+            self._current_ind += 1
+
+    def compute_mean_sequence(self, trimmed_ratio:float, winsorized_ratio:float, cosine_ratio:float):
+        """
+        Compute the mean sequence of the data using three robust methods: trimmed mean, winsorized mean and cosine tapered mean.
+        
+        Parameters:
+        trimmed_ratio (float): The proportion of values to trim for the trimmed mean calculation. (Two sided)
+        winsorized_ratio (float): The proportion of values to replace for the winsorized mean calculation. (Two sided)
+        cosine_ratio (float): The proportion of values to taper for the cosine tapered mean calculation. (Two sided)
+
+        Returns:
+        robust_mean_seq (dict): A dictionary containing the mean sequences calculated using each of the three robust methods.
+        """
+        trimmed_mean_seq = []
+        winsorized_mean_seq = []
+        cosine_mean_seq = []
+        for _ in range(self.n):
+            self.__insert_next_for__()
+            trimmed_mean_seq.append(self.__trimmed_mean(trimmed_ratio))
+            winsorized_mean_seq.append(self.__winsorized_mean(winsorized_ratio))
+            cosine_mean_seq.append(self.__cosine_tapered_mean(cosine_ratio))
+        robust_mean_seq = {
+            "trimmed": np.array(trimmed_mean_seq),
+            "winsorized": np.array(winsorized_mean_seq),
+            "cosine": np.array(cosine_mean_seq)
+        }
+        return robust_mean_seq
+
+    def __trimmed_mean(self, ratio:float):
+        """
+        Calculate the trimmed mean of the sorted data. (For a specific sorted data)
+
+        Parameters:
+        ratio (float): The proportion of values to trim.
+        
+        Returns:
+        trimmed_mean (float): The trimmed mean of the data.
+        """
+        assert isinstance(ratio, (float, int)) and 0 <= ratio <= 1, f"ratio={ratio} must be a float or float in the range [0, 1]"
+        # Compute lower and upper cutoff indices
+        lower_ind = int(np.floor(len(self._sorted_data)) * ratio)
+        upper_ind = len(self._sorted_data) - lower_ind
+        # Trim the data and compute the mean
+        trimmed_data = self._sorted_data[lower_ind:upper_ind]
+        trimmed_mean = np.mean(trimmed_data)
+        return trimmed_mean
+    
+    def __winsorized_mean(self, ratio:float):
+        """
+        Calculate the winsorized mean of the sorted data. (For a specific sorted data)
+
+        Parameters:
+        ratio (float): The proportion of values to replace.
+        
+        Returns:
+        winsorized_mean (float): The winsorized mean of the data.
+        """
+        assert isinstance(ratio, (float, int)) and 0 <= ratio <= 1, f"ratio={ratio} must be a float or float in the range [0, 1]"
+        # Create a copy of the sorted data
+        winsorized_data = self._sorted_data.copy()
+        # Compute lower and upper cutoff indices
+        lower_ind = int(np.floor(len(self._sorted_data)) * ratio)
+        upper_ind = len(self._sorted_data) - lower_ind
+        # Replace the lowest and highest values
+        winsorized_data[:lower_ind] = [self._sorted_data[lower_ind]] * lower_ind
+        winsorized_data[upper_ind:] = [self._sorted_data[upper_ind - 1]] * (len(self._sorted_data) - upper_ind) # remind that the ind start with 0
+        # winsorized_data = mstats.winsorize(self._sorted_data, limits=[ratio, ratio])
+        # Compute the mean
+        winsorized_mean = np.mean(winsorized_data)
+        return winsorized_mean
+    
+    def __cosine_tapered_mean(self, ratio:float):
+        """
+        Calculate the cosine tapered mean of the sorted data. (For a specific sorted data)
+
+        Parameters:
+        ratio (float): The proportion of values to taper.
+        
+        Returns:
+        cosine_tapered_mean (float): The cosine tapered mean of the data.
+        """
+        assert isinstance(ratio, (float, int)) and 0 <= ratio <= 1, f"ratio={ratio} must be a float or float in the range [0, 1]"
+        # Initialised the weights
+        weights = tukey(len(self._sorted_data), ratio) 
+        # Apply the weight to the sorted data
+        cosine_tapered_data = weights * self._sorted_data
+        # Compute the mean
+        cosine_tapered_mean = np.mean(cosine_tapered_data)
+        return cosine_tapered_mean
+    
+    def sliding_window_median(self, window_length:int):
+        """
+        Compute the median of a sliding window over the data. (For the whole data stream)
+
+        Parameters:
+        window_length (int): The length of the sliding window.
+        
+        Returns:
+        medians (list): The medians of each window of the whole data stream.
+        """
+        assert isinstance(window_length, int) and window_length <= self.n, f"Window length={window_length} must be an integer less than or equal to the number of observations={self.n}"
+        medians = [] # for store a list of median values
+        for i in range(self.n - window_length + 1):
+            # Compute the median of the window and add it to the list
+            window_median = np.median(self.x[i : i + window_length])
+            medians.append(window_median)
+        return medians
--- simulation_study.py
+++ simulation_study.py
@@ -6,6 +6,7 @@ import GraphGeneration
 import ARLFunc
 import GridEvaluation
 import Outliers
+import ControlChartFunc
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -20,8 +21,49 @@ from GridEvaluation import GridDataEvaluate, simulate_stream_data, stream_data_p
 importlib.reload(Outliers)
 from Outliers import OutlierInjector
 
+importlib.reload(ControlChartFunc)
+from ControlChartFunc import RobustMethods
 
+# ------------------Testing function for new class-------------------
+# Compute lower and upper cutoff indices
+from scipy.stats import trim_mean
+from scipy.stats import mstats
+from scipy.signal import tukey
+sorted_data = np.array([1,3,4,5,6,8,9,11,19])
+ratio = 0.2
+lower_ind = int(np.floor(len(sorted_data)) * ratio)
+upper_ind = len(sorted_data) - lower_ind
+# Trim the data and compute the mean
+trimmed_data = sorted_data[lower_ind:upper_ind]
+trimmed_mean = np.mean(trimmed_data)
+trim_mean(sorted_data, ratio)
+winsorized_data = mstats.winsorize(sorted_data,limits=[ratio, ratio])
+winsorized_mean = np.mean(winsorized_data)
+my_winsorized_data = sorted_data
+my_winsorized_data[:lower_ind] = sorted_data[lower_ind]
+my_winsorized_data[upper_ind:] = sorted_data[upper_ind-1]
+# Compute the mean
+winsorized_mean = np.mean(my_winsorized_data)
+# Sliding window median
+window_length = 4
+medians = [] # for store a list of median values
+for i in range(len(sorted_data) - window_length + 1):
+    window_median = np.median(sorted_data[i : i + window_length])
+    medians.append(window_median)
 
+weights = tukey(len(sorted_data), ratio) 
+# Apply the weight to the sorted data
+cosine_tapered_data = weights * sorted_data
+# Compute the mean
+cosine_tapered_mean = np.mean(cosine_tapered_data)
+
+random_data = np.random.normal(size=20)
+robust_method = RobustMethods(random_data)
+sliding_window_median = robust_method.sliding_window_median(window_length=10)
+robust_mean_seq = robust_method.compute_mean_sequence(trimmed_ratio=0.2, winsorized_ratio=0.2, cosine_ratio=0.2)
+trimmed_mean = robust_mean_seq['trimmed']
+winsorized_mean = robust_mean_seq['winsorized']
+cosine_tapered_mean = robust_mean_seq['cosine']
 # ------------------Testing function for grid_params_eval function without outlier-------------------
 # For displaying the full pd.df
 pd.set_option('display.max_columns', None)
@@ -60,7 +102,7 @@ BURNIN = 100
 cusum_params_list = [(1.50, 1.61), (1.25, 1.99), (1.00, 2.52), (0.75, 3.34), (0.50, 4.77), (0.25, 8.01)]
 ewma_params_list = [(1.00,3.090),(0.75,3.087),(0.50,3.071),(0.40,3.054),(0.30,3.023),(0.25,2.998),(0.20,2.962),(0.10,2.814),(0.05,2.615),(0.03,2.437)]
 valid_positions = ['in-control', 'out-of-control', 'both_in_and_out', 'burn-in']
-outlier_position = valid_positions[0]
+outlier_position = valid_positions[3]
 alpha = 1e-5
 outlier_ratio = 0.05
 asymmetric_ratio = 0.25
@@ -84,7 +126,7 @@ burnin = 100
 gap_size = 5
 alpha = 0.001
 valid_positions = ['in-control', 'out-of-control', 'both_in_and_out', 'burn-in']
-outlier_position = valid_positions[0]
+outlier_position = valid_positions[3]
 outlier_ratio = 0.05
 asymmetric_ratio = 0.25
 data_1 = np.append(np.random.normal(size=n_sam_bef_cp, scale=np.sqrt(variance)), 
